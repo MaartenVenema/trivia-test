4fea2f57f9a79518c91570704fbda88c7a1803ed
unknown <maartenvenema@gmail.com>
2021-06-06 15:30:38 +0200

    - cleaned up function naming and how the function where bound to the game var vs this - moved/rewrote some code to make it more readable - renamed a few functions to be more descriptive - added comments to make the code clearer to navigate - added a max amount of player which is configurable - created some more indepth tests - added more user feedback messages to make clear where the players stand - player amount - show when there is a winner

--- game.js
+++ game.js
@@ -1,10 +1,25 @@
+/*
+  Improvements i made
+  - cleaned up function naming and how the function where bound to the game var vs this
+  - moved/rewrote some code to make it more readable 
+  - renamed a few functions to be more descriptive
+  - added comments to make the code clearer to navigate
+  - added a max amount of player which is configurable
+  - created some more indepth tests
+  - added more user feedback messages to make clear where the players stand
+    - player amount
+    - show when there is a winner
+*/
 exports = typeof window !== "undefined" && window !== null ? window : global;
 
 exports.Game = function () {
+  // Set the max amount of players
+  const maxNumOfPlayers = 6;
+
   var players = new Array();
-  var places = new Array(6);
-  var purses = new Array(6);
-  var inPenaltyBox = new Array(6);
+  var places = new Array(maxNumOfPlayers);
+  var purses = new Array(maxNumOfPlayers);
+  var inPenaltyBox = new Array(maxNumOfPlayers);
 
   var popQuestions = new Array();
   var scienceQuestions = new Array();
@@ -14,39 +29,69 @@ exports.Game = function () {
   var currentPlayer = 0;
   var isGettingOutOfPenaltyBox = false;
 
-  var didPlayerWin = function () {
-    return !(purses[currentPlayer] == 6);
-  };
-
-  var currentCategory = function () {
-    if (places[currentPlayer] == 0) return "Pop";
-    if (places[currentPlayer] == 4) return "Pop";
-    if (places[currentPlayer] == 8) return "Pop";
-    if (places[currentPlayer] == 1) return "Science";
-    if (places[currentPlayer] == 5) return "Science";
-    if (places[currentPlayer] == 9) return "Science";
-    if (places[currentPlayer] == 2) return "Sports";
-    if (places[currentPlayer] == 6) return "Sports";
-    if (places[currentPlayer] == 10) return "Sports";
-    return "Rock";
-  };
-
-  this.createRockQuestion = function (index) {
-    return "Rock Question " + index;
-  };
-
+  // Populate questions
   for (var i = 0; i < 50; i++) {
     popQuestions.push("Pop Question " + i);
     scienceQuestions.push("Science Question " + i);
     sportsQuestions.push("Sports Question " + i);
-    rockQuestions.push(this.createRockQuestion(i));
+    rockQuestions.push("Rock Question " + i);
   }
 
-  this.isPlayable = function (howManyPlayers) {
-    return howManyPlayers >= 2;
+  this.getMaxNumOfPlayers = function () {
+    return maxNumOfPlayers;
+  }
+
+  this.getPlayers = function () {
+    return players;
+  }
+
+
+  // If a player has 6 coins he/she wins
+  this.didPlayerWin = function () {
+    return !(purses[currentPlayer] == 6);
   };
 
-  this.add = function (playerName) {
+  // Create question mapping / game board
+  this.currentCategory = function () {
+    if (
+      places[currentPlayer] == 0 ||
+      places[currentPlayer] == 4 ||
+      places[currentPlayer] == 8
+    ) {
+      return "Pop";
+    }
+
+    if (
+      places[currentPlayer] == 1 ||
+      places[currentPlayer] == 5 ||
+      places[currentPlayer] == 9
+    ) {
+      return "Science";
+    }
+
+    if (
+      places[currentPlayer] == 2 ||
+      places[currentPlayer] == 6 ||
+      places[currentPlayer] == 10
+    ) {
+      return "Sports";
+    }
+
+    return "Rock";
+  };
+
+  // Return the amount of players
+  this.howManyPlayers = function () {
+    return players.length;
+  };
+
+  // Check if the game can start
+  this.isPlayable = function () {
+    return this.howManyPlayers() >= 2 && this.howManyPlayers() <= maxNumOfPlayers;
+  };
+
+  // Add a player
+  this.addPlayer = function (playerName) {
     players.push(playerName);
     places[this.howManyPlayers() - 1] = 0;
     purses[this.howManyPlayers() - 1] = 0;
@@ -58,21 +103,20 @@ exports.Game = function () {
     return true;
   };
 
-  this.howManyPlayers = function () {
-    return players.length;
+  // Ask a question and remove the question from the questions array
+  this.askQuestion = function () {
+    if (this.currentCategory() == "Pop") console.log(popQuestions.shift());
+    if (this.currentCategory() == "Science") console.log(scienceQuestions.shift());
+    if (this.currentCategory() == "Sports") console.log(sportsQuestions.shift());
+    if (this.currentCategory() == "Rock") console.log(rockQuestions.shift());
   };
 
-  var askQuestion = function () {
-    if (currentCategory() == "Pop") console.log(popQuestions.shift());
-    if (currentCategory() == "Science") console.log(scienceQuestions.shift());
-    if (currentCategory() == "Sports") console.log(sportsQuestions.shift());
-    if (currentCategory() == "Rock") console.log(rockQuestions.shift());
-  };
-
-  this.roll = function (roll) {
+  // Roll the dice for a player
+  this.rollDice = function (roll) {
     console.log(players[currentPlayer] + " is the current player");
     console.log("They have rolled a " + roll);
 
+    // If the player has answered wronly previously they are put in the penalty box and must roll a odd number to get out
     if (inPenaltyBox[currentPlayer]) {
       if (roll % 2 != 0) {
         isGettingOutOfPenaltyBox = true;
@@ -88,8 +132,8 @@ exports.Game = function () {
         console.log(
           players[currentPlayer] + "'s new location is " + places[currentPlayer]
         );
-        console.log("The category is " + currentCategory());
-        askQuestion();
+        console.log("The category is " + this.currentCategory());
+        this.askQuestion();
       } else {
         console.log(
           players[currentPlayer] + " is not getting out of the penalty box"
@@ -97,6 +141,7 @@ exports.Game = function () {
         isGettingOutOfPenaltyBox = false;
       }
     } else {
+      // If not in the penalty box a player gets to answer a question
       places[currentPlayer] = places[currentPlayer] + roll;
       if (places[currentPlayer] > 11) {
         places[currentPlayer] = places[currentPlayer] - 12;
@@ -105,11 +150,12 @@ exports.Game = function () {
       console.log(
         players[currentPlayer] + "'s new location is " + places[currentPlayer]
       );
-      console.log("The category is " + currentCategory());
-      askQuestion();
+      console.log("The category is " + this.currentCategory());
+      this.askQuestion();
     }
   };
 
+  // If the answer was correct
   this.wasCorrectlyAnswered = function () {
     if (inPenaltyBox[currentPlayer]) {
       if (isGettingOutOfPenaltyBox) {
@@ -122,7 +168,7 @@ exports.Game = function () {
             " Gold Coins."
         );
 
-        var winner = didPlayerWin();
+        var winner = this.didPlayerWin();
         currentPlayer += 1;
         if (currentPlayer == players.length) currentPlayer = 0;
 
@@ -143,7 +189,7 @@ exports.Game = function () {
           " Gold Coins."
       );
 
-      var winner = didPlayerWin();
+      var winner = this.didPlayerWin();
 
       currentPlayer += 1;
       if (currentPlayer == players.length) currentPlayer = 0;
@@ -152,6 +198,7 @@ exports.Game = function () {
     }
   };
 
+  // If the answer was wrong
   this.wrongAnswer = function () {
     console.log("Question was incorrectly answered");
     console.log(players[currentPlayer] + " was sent to the penalty box");
@@ -163,20 +210,36 @@ exports.Game = function () {
   };
 };
 
+// Initiate game
+var game = new Game();
 var notAWinner = false;
 
-var game = new Game();
+// Mock gameplay
+game.addPlayer("Chet");
+game.addPlayer("Pat");
+game.addPlayer("Sue");
 
-game.add("Chet");
-game.add("Pat");
-game.add("Sue");
+// Check if the game is playable. The game is currently set to have between 2 and 6 players
+if(game.isPlayable()) {
+  do {
+    // Roll a dice between 1 and 6
+    game.rollDice(Math.floor(Math.random() * 6) + 1);
 
-do {
-  game.roll(Math.floor(Math.random() * 6) + 1);
+    // Randomly guess correct or wrong
+    if (Math.floor(Math.random() * 10) == 7) {
+      notAWinner = game.wrongAnswer();
+    } else {
+      notAWinner = game.wasCorrectlyAnswered();
+    }
 
-  if (Math.floor(Math.random() * 10) == 7) {
-    notAWinner = game.wrongAnswer();
-  } else {
-    notAWinner = game.wasCorrectlyAnswered();
-  }
-} while (notAWinner);
+    // If a user collects 6 coins he/she is the winner. Communicate this with the player
+    if (!notAWinner) {
+      // We should let the players know there is a winner
+      console.log("we have a winner!!!!");
+    }
+  } while (notAWinner);
+} else {
+  // We should let the players know if the game is not setup correctly
+  const numPlayersString = game.howManyPlayers() < 2 ? 'There is currently 1 player' : `There are currently ${game.howManyPlayers()} players`;
+  console.log(`The game cannot start. ${numPlayersString}. The minimum amount of players is 2 and the max 6.`);
+}
--- spec/game.spec.js
+++ spec/game.spec.js
@@ -1,15 +1,96 @@
 require('../game.js');
 
-describe("The test environment", function() {
-  it("should pass", function() {
-    expect(true).toBe(true);
+describe("Check if the game setups correctly", function() {
+  beforeEach(function() {
+     this.game = new Game();
+  });
+
+  afterEach(function() {
+    this.game = null;
   });
 
   it("should access game", function() {
-    expect(Game).toBeDefined();
+    // expect(Game).toBeDefined();
+    expect(this.game).toBeDefined();
+  });
+
+  it("should be able to add a player", function() {
+    expect(this.game.addPlayer("Chet")).toBe(true);
+  });
+
+  it("should not be playable with 1 player", function() {
+    this.game.addPlayer("Chet");
+    expect(this.game.isPlayable()).toBe(false);
+  });
+
+  it("Should should be playable with 2 players", function() {
+    this.game.addPlayer("Chet");
+    this.game.addPlayer("Pat");
+    expect(this.game.isPlayable()).toBe(true);
+  });
+
+  it("Should be playable with the max number of players set for the game", function() {
+    const maxNumOfPlayers = this.game.getMaxNumOfPlayers();
+
+    console.log('max amount of players is set to '+maxNumOfPlayers)
+
+    for (let index = 0; index < maxNumOfPlayers; index++) {
+      this.game.addPlayer("player_"+index);
+    }
+    expect(this.game.isPlayable()).toBe(true);
+  });
+
+  it("Should not be playable with more then maxNumOfPlayers", function() {
+    const maxNumOfPlayers = this.game.getMaxNumOfPlayers();
+
+    console.log('max amount of players is set to '+maxNumOfPlayers)
+
+    for (let index = 0; index < maxNumOfPlayers+1; index++) {
+      this.game.addPlayer("player_"+index);
+    }
+    expect(this.game.isPlayable()).toBe(false);
   });
 });
 
-describe("Your specs...", function() {
-  // it ...
+  // This could be approved upon. It could/should check the diffrent steps of the game are run correctly but my time is almost up ;)
+describe("Check if the game can run a mock game", function() {
+  beforeEach(function() {
+     this.game = new Game();
+  });
+
+  afterEach(function() {
+    this.game = null;
+  });
+
+  it("Should be able to run a full mock game", function() {
+    var notAWinner = false;
+
+    this.game.addPlayer("Chet");
+    this.game.addPlayer("Pat");
+    this.game.addPlayer("Sue");
+
+    // If the game can run play a mock game with the added players
+    if(this.game.isPlayable()) {
+      do {
+        // Roll a dice between 1 and 6
+        this.game.rollDice(Math.floor(Math.random() * 6) + 1);
+
+        // Randomly guess correct or wrong
+        if (Math.floor(Math.random() * 10) == 7) {
+          notAWinner = this.game.wrongAnswer();
+        } else {
+          notAWinner = this.game.wasCorrectlyAnswered();
+        }
+
+        // If a user collects 6 coins he/she is the winner. Communicate this with the player
+        if (!notAWinner) {
+          console.log("we have a winner!");
+          expect(true).toBe(true);
+        }
+      } while (notAWinner);
+    } else {
+      const numPlayersString = this.game.howManyPlayers() < 2 ? 'There is currently 1 player' : `There are currently ${game.howManyPlayers()} players`;
+      console.log(`The game cannot start. ${numPlayersString}. The minimum amount of players is 2 and the max 6.`);
+    }
+  });
 });
